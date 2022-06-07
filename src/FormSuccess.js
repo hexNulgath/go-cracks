b6e24765adab268d8376a870569e288e6198628a
hexNulgath <machadoignacio@hotmail.com>
2022-06-06 21:58:36 -0300

    1

--- src/FormSuccess.js
+++ src/FormSuccess.js
@@ -1,35 +1,40 @@
-import React from 'react';
 import axios from 'axios';
 
 const FormSuccess = () => {
-  const cached = getForm();
-
   function getForm() {
     let f = window.localStorage.getItem('form');
     if (f) return JSON.parse(f);
   }
+  const cached = getForm();
   const total = cached.wages * cached.employees * 300 * parseInt(cached.horas);
+
   function sendUserData() {
     const options = {
-      method: 'GET',
-      url: 'http://localhost:8000/data',
+      method: 'get',
+      url: 'https://go-cracks.herokuapp.com/data',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+        'Access-Control-Allow-Credentials': 'true',
+      },
       params: { total: total, body: cached },
     };
-
     axios
       .request(options)
-      .then((response) => {
-        sendUserData(response.data);
+      .then(function (response) {
+        console.log(response.data);
+        window.location.replace(response.data);
       })
       .catch((error) => {
         console.error(error);
       });
   }
-
   sendUserData();
   return (
-    <div className="checkout-btn">
-      <p>'el coste total es {total}'</p>
+    <div>
+      <h1>please wait</h1>
     </div>
   );
 };
