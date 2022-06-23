73792641aabb75b284d230f65c3d531e9039e8c0
hexNulgath <machadoignacio@hotmail.com>
2022-06-23 01:42:34 -0300

    1

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ function App() {
 
   return (
     <div className="container">
-      <nav class="navbar navbar-expand-lg bg-light">
+      <nav class="navbar navbar-expand-lg" id="navbar">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Go Cracks
+          <a class="navbar-brand" href="https://www.gocracks.uy/">
+            <img src="/images/logo.png" alt="" id="logoNavbar" />
           </a>
           <button
             class="navbar-toggler"
@@ -31,35 +31,104 @@ function App() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon "></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">
-                  Inicio
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Nuestro objetivo
-                </a>
-              </li>
-              <li class="nav-item ">
-                <a class="nav-link" href="#" aria-expanded="false">
-                  Cracks
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active">Empresas</a>
-              </li>
-            </ul>
+          <div class="d-flex flex-row-reverse">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a
+                    class="nav-link"
+                    aria-current="page"
+                    href="https://www.gocracks.uy/"
+                  >
+                    Inicio
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://www.gocracks.uy/">
+                    Nuestro objetivo
+                  </a>
+                </li>
+                <li class="nav-item ">
+                  <a
+                    class="nav-link"
+                    href="https://www.gocracks.uy/"
+                    aria-expanded="false"
+                  >
+                    Cracks
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link active"
+                    id="active"
+                    href="https://www.gocracks.uy/"
+                  >
+                    Empresas
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
       <div className="App">
         {!isSubmitted ? <Form submitForm={submitForm} /> : <FormSuccess />}
       </div>
+      <footer class="text-center text-lg-start" id="footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-8 mb-4 mb-md-0">
+              <ul class="list-unstyled mb-0">
+                <li>
+                  <a href="https://www.gocracks.uy/">Inicio</a>
+                </li>
+                <li>
+                  <a href="https://www.gocracks.uy/">Nuestro Objetivo</a>
+                </li>
+                <li>
+                  <a href="https://www.gocracks.uy/">Cracks</a>
+                </li>
+                <li>
+                  <a href="https://www.gocracks.uy/" id="footerActive">
+                    Empresas
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-lg-2 col-md-2 mb-4 mb-md-0">
+              <ul class="list-unstyled mb-0">
+                <br />
+                <li>
+                  <a href="https://www.gocracks.uy/">Facebook</a>
+                </li>
+                <li>
+                  <a href="https://www.gocracks.uy/">Instagram</a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-lg-2 col-md-2 mb-4 mb-md-0">
+              <ul class="list-unstyled">
+                <br />
+                <li>
+                  <p>123-456-7890</p>
+                </li>
+                <li>
+                  <p>info@cracks.com</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="text-center " id="copyright">
+          © 2022 GoCracks. Todos los derechos reservados. Diseñado por Studio
+          Morai.
+        </div>
+      </footer>
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -2,6 +2,7 @@ import React from 'react';
 import useForm from './useForm';
 import validate from './validateInfo';
 import './Form.css';
+import { Row } from 'react-bootstrap';
 
 const Form = ({ submitForm }) => {
   const { handleChange, values, handleSubmit, errors } = useForm(
@@ -9,230 +10,264 @@ const Form = ({ submitForm }) => {
     validate
   );
   return (
-    <div className="form-content-right">
-      <h1>Solo un paso más...</h1>
+    <div className="form-content-right" id="Form">
+      <h2>
+        Solo <span>un paso</span> más...
+      </h2>
       <p>
         Envíanos tus datos y la fecha para la cual necesitas contratar un Crack
         y nuestro equipo de soporte se encargará de la selección y de enviarte
         todos los detalles pertinentes.{' '}
       </p>
-      <em></em>
+      <br />
       <form className="form" onSubmit={handleSubmit}>
-        <div className="form-inputs">
-          <label htmlFor="username" className="form-label">
-            Nombre*
-          </label>
-          <br />
-          <input
-            id="username"
-            type="text"
-            name="username"
-            className="form-input"
-            placeholder="Nombre"
-            value={values.username}
-            onChange={handleChange}
-          />
-          {errors.username && <p>{errors.username}</p>}
+        <div className="row">
+          <div className="col">
+            <div className="form-inputs">
+              <label htmlFor="username" className="form-label">
+                Nombre*
+              </label>
+              <br />
+              <input
+                id="username"
+                type="text"
+                name="username"
+                className="form-input"
+                placeholder="Nombre"
+                value={values.username}
+                onChange={handleChange}
+              />
+              {errors.username && <p>{errors.username}</p>}
+            </div>
+          </div>
+          <div className="col">
+            {' '}
+            <div className="form-inputs">
+              <label htmlFor="companyName" className="form-label">
+                Empresa*
+              </label>
+              <br />
+              <input
+                id="companyName"
+                type="text"
+                name="companyName"
+                className="form-input"
+                placeholder="Empresa"
+                value={values.companyName}
+                onChange={handleChange}
+              />
+              {errors.companyName && <p>{errors.companyName}</p>}
+            </div>
+          </div>
+          <div className="col">
+            {' '}
+            <div className="form-inputs">
+              <label htmlFor="email" className="form-label">
+                Email*
+              </label>
+              <br />
+              <input
+                id="email"
+                type="email"
+                name="email"
+                className="form-input"
+                placeholder="Email"
+                value={values.email}
+                onChange={handleChange}
+              />
+              {errors.email && <p>{errors.email}</p>}
+            </div>
+          </div>
         </div>
-
-        <div className="form-inputs">
-          <label htmlFor="companyName" className="form-label">
-            Empresa*
-          </label>
-          <br />
-          <input
-            id="companyName"
-            type="text"
-            name="companyName"
-            className="form-input"
-            placeholder="Empresa"
-            value={values.companyName}
-            onChange={handleChange}
-          />
-          {errors.companyName && <p>{errors.companyName}</p>}
+        <div className="row">
+          <div className="col">
+            <div className="form-inputs">
+              <label htmlFor="serviceDate" className="form-label">
+                Fecha del servicio*
+              </label>
+              <br />
+              <input
+                id="serviceDate"
+                type="date"
+                name="serviceDate"
+                className="form-input"
+                placeholder="Fecha del servicio"
+                value={values.serviceDate}
+                onChange={handleChange}
+              />
+              {errors.serviceDate && <p>{errors.serviceDate}</p>}
+            </div>
+          </div>
+          <div className="col">
+            <div className="form-inputs">
+              <label htmlFor="startTime" className="form-label">
+                Hora de inicio del servicio*
+              </label>
+              <br />
+              <input
+                id="startTime"
+                type="time"
+                name="startTime"
+                className="form-input"
+                placeholder="Hora de inicio del servicio"
+                value={values.startTime}
+                onChange={handleChange}
+              />
+              {errors.startTime && <p>{errors.startTime}</p>}
+            </div>
+          </div>
+          <div className="col">
+            {' '}
+            <div className="form-inputs">
+              Cantidad de horas* <br />
+              <input
+                id="ochoHoras"
+                type="radio"
+                name="horas"
+                className="form-input"
+                value="8"
+                onChange={handleChange}
+              />
+              <label htmlFor="ochoHoras" className="form-label">
+                8h
+              </label>
+              <input
+                id="seisHoras"
+                type="radio"
+                name="horas"
+                className="form-input"
+                value="6"
+                onChange={handleChange}
+              />
+              <label htmlFor="seisHoras" className="form-label">
+                6h
+              </label>
+              {errors.horas && <p>{errors.horas}</p>}
+            </div>
+          </div>
         </div>
-
-        <div className="form-inputs">
-          <label htmlFor="email" className="form-label">
-            Email*
-          </label>
-          <br />
-          <input
-            id="email"
-            type="email"
-            name="email"
-            className="form-input"
-            placeholder="Email"
-            value={values.email}
-            onChange={handleChange}
-          />
-          {errors.email && <p>{errors.email}</p>}
+        <div className="row">
+          <div className="col">
+            <div className="form-inputs">
+              <label htmlFor="wages" className="form-label">
+                Cantidad de jornales deseados*
+              </label>
+              <br />
+              <input
+                id="wages"
+                type="number"
+                name="wages"
+                className="form-input"
+                placeholder="Cantidad de jornales deseados"
+                value={values.wages}
+                onChange={handleChange}
+              />
+              {errors.wages && <p>{errors.wages}</p>}
+            </div>
+          </div>
+          <div className="col">
+            {' '}
+            <div className="form-inputs">
+              <label htmlFor="employees" className="form-label">
+                Cantidad de Cracks a contratar*
+              </label>
+              <br />
+              <input
+                id="employees"
+                type="number"
+                name="employees"
+                className="form-input"
+                placeholder="Cantidad de Cracks a contratar"
+                value={values.employees}
+                onChange={handleChange}
+              />
+              {errors.employees && <p>{errors.employees}</p>}
+            </div>
+          </div>
+          <div className="col">
+            <div className="form-inputs">
+              <label htmlFor="rut" className="form-label">
+                RUT
+              </label>
+              <br />
+              <input
+                id="rut"
+                type="text"
+                name="rut"
+                className="form-input"
+                placeholder="RUT"
+                value={values.rut}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
         </div>
-
-        <div className="form-inputs">
-          <label htmlFor="serviceDate" className="form-label">
-            Fecha del servicio*
-          </label>
-          <br />
-          <input
-            id="serviceDate"
-            type="date"
-            name="serviceDate"
-            className="form-input"
-            placeholder="Fecha del servicio"
-            value={values.serviceDate}
-            onChange={handleChange}
-          />
-          {errors.serviceDate && <p>{errors.serviceDate}</p>}
+        <div className="row">
+          <div className="col-8">
+            <div className="row">
+              <div className="col">
+                <div className="form-inputs">
+                  <label htmlFor="phone" className="form-label">
+                    Celular de contacto*
+                  </label>{' '}
+                  <br />
+                  <input
+                    id="phone"
+                    type="tel"
+                    name="phone"
+                    className="form-input"
+                    placeholder="Celular de contacto"
+                    value={values.phone}
+                    onChange={handleChange}
+                  />{' '}
+                  {errors.phone && <p>{errors.phone}</p>}
+                </div>
+              </div>
+              <div className="col">
+                <div className="form-inputs">
+                  <label htmlFor="adress" className="form-label">
+                    Direccion del servicio*
+                  </label>{' '}
+                  <br />
+                  <input
+                    id="adress"
+                    type="text"
+                    name="adress"
+                    className="form-input"
+                    placeholder="Direccion del servicio"
+                    value={values.adress}
+                    onChange={handleChange}
+                  />{' '}
+                  {errors.adress && <p>{errors.adress}</p>}
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="form-inputs">
+                <label htmlFor="message" className="form-label">
+                  Comentarios
+                </label>
+                <br />
+                <input
+                  id="message"
+                  type="text"
+                  name="message"
+                  maxLength="450"
+                  className="form-input"
+                  placeholder="Comentarios"
+                  value={values.message}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <button className="form-input-btn" type="submit">
+                Solicitar Crack
+              </button>
+            </div>
+          </div>
+          <div className="col"></div>
         </div>
-
-        <div className="form-inputs">
-          <label htmlFor="startTime" className="form-label">
-            Hora de inicio del servicio*
-          </label>
-          <br />
-          <input
-            id="startTime"
-            type="time"
-            name="startTime"
-            className="form-input"
-            placeholder="Hora de inicio del servicio"
-            value={values.startTime}
-            onChange={handleChange}
-          />
-          {errors.startTime && <p>{errors.startTime}</p>}
-        </div>
-        <div className="form-inputs">
-          Cantidad de horas* <br />
-          <input
-            id="ochoHoras"
-            type="radio"
-            name="horas"
-            className="form-input"
-            value="8"
-            onChange={handleChange}
-          />
-          <label htmlFor="ochoHoras" className="form-label">
-            8h
-          </label>
-          <input
-            id="seisHoras"
-            type="radio"
-            name="horas"
-            className="form-input"
-            value="6"
-            onChange={handleChange}
-          />
-          <label htmlFor="seisHoras" className="form-label">
-            6h
-          </label>
-          {errors.horas && <p>{errors.horas}</p>}
-        </div>
-
-        <div className="form-inputs">
-          <label htmlFor="wages" className="form-label">
-            Cantidad de jornales deseados*
-          </label>
-          <br />
-          <input
-            id="wages"
-            type="number"
-            name="wages"
-            className="form-input"
-            placeholder="Cantidad de jornales deseados"
-            value={values.wages}
-            onChange={handleChange}
-          />
-          {errors.wages && <p>{errors.wages}</p>}
-        </div>
-
-        <div className="form-inputs">
-          <label htmlFor="employees" className="form-label">
-            Cantidad de Cracks a contratar*
-          </label>
-          <br />
-          <input
-            id="employees"
-            type="number"
-            name="employees"
-            className="form-input"
-            placeholder="Cantidad de Cracks a contratar"
-            value={values.employees}
-            onChange={handleChange}
-          />
-          {errors.employees && <p>{errors.employees}</p>}
-        </div>
-
-        <div className="form-inputs">
-          <label htmlFor="rut" className="form-label">
-            RUT
-          </label>
-          <br />
-          <input
-            id="rut"
-            type="text"
-            name="rut"
-            className="form-input"
-            placeholder="RUT"
-            value={values.rut}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div className="form-inputs">
-          <label htmlFor="phone" className="form-label">
-            Celular de contacto*
-          </label>{' '}
-          <br />
-          <input
-            id="phone"
-            type="tel"
-            name="phone"
-            className="form-input"
-            placeholder="Celular de contacto"
-            value={values.phone}
-            onChange={handleChange}
-          />{' '}
-          {errors.phone && <p>{errors.phone}</p>}
-        </div>
-
-        <div className="form-inputs">
-          <label htmlFor="adress" className="form-label">
-            Direccion del servicio*
-          </label>{' '}
-          <br />
-          <input
-            id="adress"
-            type="text"
-            name="adress"
-            className="form-input"
-            placeholder="Direccion del servicio"
-            value={values.adress}
-            onChange={handleChange}
-          />{' '}
-          {errors.adress && <p>{errors.adress}</p>}
-        </div>
-
-        <div className="form-inputs">
-          <label htmlFor="message" className="form-label">
-            Comentarios
-          </label>
-          <br />
-          <input
-            id="message"
-            type="text"
-            name="message"
-            maxLength="450"
-            className="form-input"
-            placeholder="Comentarios"
-            value={values.message}
-            onChange={handleChange}
-          />
-        </div>
-
-        <button className="form-input-btn" type="submit">
-          Solicitar Crack
-        </button>
       </form>
     </div>
   );
