984fa2bacfbe093e330e35dd3301752d02c943f2
hexNulgath <machadoignacio@hotmail.com>
2022-07-06 19:08:28 -0300

    12

--- src/FormSuccess.js
+++ src/FormSuccess.js
@@ -19,7 +19,7 @@ const FormSuccess = () => {
   function sendUserData() {
     const options = {
       method: 'get',
-      url: 'https://go-cracks.herokuapp.com/data',
+      url: 'http://server-env.eba-yxc8tdwq.us-east-1.elasticbeanstalk.com/data',
       headers: {
         'content-type': 'application/x-www-form-urlencoded',
         'Access-Control-Allow-Origin': '*',
